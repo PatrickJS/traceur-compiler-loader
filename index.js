68e6b54c48b41eab1a5451d49699f8cca62d3761
gdi2290 <github@gdi2290.com>
2015-03-08 15:39:33 -0700

    chore: relative moduleName

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var loaderUtils = require('loader-utils');
 var SourceNode = require("source-map").SourceNode;
 var SourceMapConsumer = require("source-map").SourceMapConsumer;
 var os = require('os');
+var path = require('path');
 var traceur = require('traceur');
 
 var defaults = {
@@ -18,13 +19,14 @@ module.exports = function(source, originalSourceMap) {
   var filename = loaderUtils.getRemainingRequest(this);
   var content = source;
   var map;
-  var options = {};
   var result;
   var runtime;
   var imports;
+  var moduleName = filename;
 
   // Process query and setup options/defaults/forced for Traceur
-  extend(options, defaults, loaderUtils.parseQuery(this.query));
+  var options = extend({}, defaults, loaderUtils.parseQuery(this.query));
+
 
   Object.keys(options).forEach(function(key) {
     switch(options[key]) {
@@ -46,8 +48,22 @@ module.exports = function(source, originalSourceMap) {
 
   // relative module names
   if (options.moduleName) {
-    options.moduleName = filename; //loaderUtils.urlToRequest(this.resourcePath, this.context);
+    var interpolateName = options.interpolateName || this.interpolateName;
+    if (interpolateName) {
+      moduleName = loaderUtils.urlToRequest(this, interpolateName, {
+        content: source,
+        context: this.context,
+        regExp: options.regExp || null
+      });
+    } else {
+      var directory = '';
+      var resourcePath = this.resourcePath;
+      directory = path.relative(this.context, resourcePath + "_").replace(/\\/g, "/").replace(/\.\.(\/)?/g, "_$1");
+      directory = directory.substr(0, directory.length-1);
+      moduleName = directory;
+    }
   }
+
 
   // Move runtime option from options to variable
   runtime = options.runtime;
@@ -67,8 +83,10 @@ module.exports = function(source, originalSourceMap) {
   // Parse code through Traceur
   try {
     delete options.runtime;
-    var compiler = new traceur.NodeCompiler(options);
-    result = compiler.compile(content, filename);
+    // console.log('options.moduleName:', options.moduleName);
+    var compiler = new traceur.NodeCompiler(options, this.context);
+    result = compiler.compile(content, moduleName);
+    // console.log('result:', result);
 
     // Include runtime after compilation due to generators hoisting the
     // runtime usage to the very top
